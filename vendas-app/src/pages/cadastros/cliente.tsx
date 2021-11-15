2acdb982a5eabad96a18507fe821b46ec22d94d1
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2021-11-15 20:24:34 -0300

    Primeiras Paginas Next.js

---
+++ vendas-app/src/pages/cadastros/cliente.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Cliente : NextPage = () => {
+    return (
+        <div className="cliente-cadastro">
+            <h2>Cadastro de clientes</h2>
+        </div>
+    )
+}
+
+export default Cliente
---
+++ vendas-app/src/pages/cadastros/hello.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const Hello : NextPage =  () => (
+    <div>
+        <h2>Hello World</h2>
+    </div>
+)
+
+export default Hello
